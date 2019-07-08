e58aabff765741f2ee30a4c6721e0165a8f5e269
mireiaGomariz <mireiapgomariz@gmail.com>
2019-07-08 12:02:58 +0200

    TS

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-"use strict";
-function saludar(nombre) {
-    console.log("Hola " + nombre);
-}
-var wolverine = {
-    nombre: "Logan"
+// let mensaje = "hola";
+//
+// if( true ) {
+//
+//   let mensaje = "adios";
+//
+// }
+//
+// console.log( mensaje );
+// const OPCIONES = "todas";
+//
+//
+// if( true ) {
+//   const OPCIONES = "ninguna";
+// }
+var nombre = "Peter";
+var numero = 124;
+var booleano = true;
+var hoy = new Date();
+hoy = new Date('2020-10-21');
+var cualquiera;
+var spiderman = {
+    nombre: "Mireia",
+    edad: 20
 };
-saludar(wolverine.nombre);
--- app.ts
+++ app.ts
@@ -1,13 +1,32 @@
-function saludar( nombre:string ) {
+// let mensaje = "hola";
+//
+// if( true ) {
+//
+//   let mensaje = "adios";
+//
+// }
+//
+// console.log( mensaje );
 
-  console.log("Hola " + nombre);
 
+// const OPCIONES = "todas";
+//
+//
+// if( true ) {
+//   const OPCIONES = "ninguna";
+// }
+
+let nombre:string = "Peter";
+let numero:number = 124;
+let booleano:boolean = true;
+
+let hoy = new Date();
+
+hoy = new Date('2020-10-21');
+
+let cualquiera:any;
+
+let spiderman = {
+  nombre: "Mireia",
+  edad: 20
 }
-
-
-var wolverine = {
-  nombre: "Logan"
-};
-
-
-saludar(wolverine.nombre);
