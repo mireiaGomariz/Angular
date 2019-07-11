8f130d3d3df66fefe23450664f0cda5778a4102b
mireiaGomariz <mireiapgomariz@gmail.com>
2019-07-11 16:23:48 +0200

    TS

--- app.js
+++ app.js
@@ -161,10 +161,17 @@
 // enviarCuartel(wolverine);
 //CLASES BASICAS EN TypeScript
 var Avenger = /** @class */ (function () {
-    function Avenger() {
-        console.log("Se ha ejecutado el constructor");
+    function Avenger(nombre, equipo, nombreReal) {
+        this.nombre = "Sin nombre";
+        this.equipo = undefined;
+        this.nombreReal = undefined;
+        this.puedePelear = false;
+        this.peleasGanadas = 0;
+        this.nombre = nombre;
+        this.equipo = equipo;
+        this.nombreReal = nombreReal;
     }
     return Avenger;
 }());
-var antman = new Avenger();
+var antman = new Avenger("Antman", "cap", "Scott Lang");
 console.log(antman);
--- app.ts
+++ app.ts
@@ -187,18 +187,20 @@
 
 class Avenger {
 
-  nombre:string;
-  equipo:string;
-  nombreReal:string;
+  nombre:string = "Sin nombre";
+  equipo:string = undefined;
+  nombreReal:string = undefined;
 
-  puedePelear:boolean;
-  peleasGanadas:number;
+  puedePelear:boolean = false;
+  peleasGanadas:number = 0;
 
-  constructor(){
-    console.log("Se ha ejecutado el constructor");
+  constructor( nombre:string, equipo:string, nombreReal:string ){
+    this.nombre = nombre;
+    this.equipo = equipo;
+    this.nombreReal = nombreReal;
   }
 }
 
-let antman:Avenger = new Avenger ();
+let antman:Avenger = new Avenger ("Antman", "cap", "Scott Lang");
 
 console.log(antman);
